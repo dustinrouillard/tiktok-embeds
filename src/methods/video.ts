b65c7c807370f6db18c6301085554c86695ff009
Dustin Rouillard <code@dstn.to>
2021-08-25 19:23:53 -0600

    Change response to redirect instead

--- src/methods/video.ts
+++ src/methods/video.ts
@@ -1,34 +1,45 @@
 import { proxyAsset, videoFromIdOrWeb } from "../helpers/video";
 import { RequestParams } from "../types/Request";
+import { TikTokData } from "../types/Tiktok";
 import { generateMeta } from "../utils/meta";
 import { parseUserAgent } from "../utils/useragent";
 
 export async function VideoFromShare(request: Request, params: RequestParams<{ id: string }>, args?: any[]): Promise<Response> {
   const Link = new URL(request.url);
   const path = Link.pathname.endsWith('/') ? Link.pathname.slice(0, -1) : Link.pathname;
-  const url = await videoFromIdOrWeb(params.id);
 
-  if (!args || !args[0]) {
+  let url: { address: string; cookie: string; data: TikTokData } | undefined;
+  if (path.endsWith('.mp4')) url = await videoFromIdOrWeb(params.id);
+
+  if (!path.endsWith('.mp4')) {
     const type = parseUserAgent(request.headers.get('user-agent') as string);
-    if (['discord', 'telegram'].includes(type)) return generateMeta(type, Link.origin, path, url.data);
+    if (['discord', 'telegram'].includes(type)) {
+      url = await videoFromIdOrWeb(params.id);
+      return generateMeta(type, Link.origin, path, url.data);
+    }
   }
 
-  const proxied = await proxyAsset(url.address, url.cookie);
-  return proxied;
+  if (url) return await proxyAsset(url.address, url.cookie);
+  return Response.redirect(`${Link.origin}${path}/video.mp4`, 302);
 }
 
 export async function VideoFromUser(request: Request, params: RequestParams<{ user: string; id: string }>, args?: any[]): Promise<Response> {
   const Link = new URL(request.url);
   const path = Link.pathname.endsWith('/') ? Link.pathname.slice(0, -1) : Link.pathname;
-  const url = await videoFromIdOrWeb(`${params.user}/video/${params.id}`);
 
-  if (!args || !args[0]) {
+  let url: { address: string; cookie: string; data: TikTokData } | undefined;
+  if (path.endsWith('.mp4')) url = await videoFromIdOrWeb(`${params.user}/video/${params.id}`);
+
+  if (!path.endsWith('.mp4')) {
     const type = parseUserAgent(request.headers.get('user-agent') as string);
-    if (['discord', 'telegram'].includes(type)) return generateMeta(type, Link.origin, path, url.data);
+    if (['discord', 'telegram'].includes(type)) {
+      url = await videoFromIdOrWeb(`${params.user}/video/${params.id}`);
+      return generateMeta(type, Link.origin, path, url.data);
+    }
   }
 
-  const proxied = await proxyAsset(url.address, url.cookie);
-  return proxied;
+  if (url) return await proxyAsset(url.address, url.cookie);
+  return Response.redirect(`${Link.origin}${path}/video.mp4`, 302);
 }
 
 export async function ThumbnailFromShare(request: Request, params: RequestParams<{ id: string }>, args?: any[]): Promise<Response> {
